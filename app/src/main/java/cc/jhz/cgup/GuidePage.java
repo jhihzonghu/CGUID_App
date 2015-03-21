ffaa8ab1af8e37192bc279fceee38ea0f5692ee5
jhihzonghu <need.student@gmail.com>
2015-03-22 00:39:05 +0800

    home2013233_GuiderFunctionPage

--- app/src/main/java/cc/jhz/cgup/GuidePage.java
+++ app/src/main/java/cc/jhz/cgup/GuidePage.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -12,11 +14,11 @@ import android.widget.Toast;
 /**
  * Created by Home on 2015/3/21.
  */
-public class GuidePage extends Activity {
+public class GuidePage extends FragmentActivity {
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
-        setContentView(R.layout.guidepage);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.guidepage);
         Button skipButton = (Button)findViewById(R.id.skipButton);
         Button nextButton = (Button)findViewById(R.id.nextButton);
 
